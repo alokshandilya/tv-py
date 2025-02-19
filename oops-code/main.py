e86c666ba45d2e58e2e15b2cccb7d5b1209a6065
Alok Shandilya <alok.shandilya10@gmail.com>
2025-02-19 15:33:47 +0530

    refactor: minor change
    - child class using parent class method
      - nothing special

--- oops-code/main.py
+++ oops-code/main.py
@@ -7,12 +7,13 @@ print(Item.all, end="\n\n")
 
 phone = Phone("Nokia 5.1 Plus", 10000, 10, 3)
 
-phone.name = "iphone"
-
 item = Item("fokat", 100, 10)
 print(item.price)
 
 item.apply_increment(0.2)
 print(item.price)
+
+phone.apply_increment(0.1)
+print(phone.price)
 
 item.send_email()
